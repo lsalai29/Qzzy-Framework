f49821789033551e49bcf57de3d77e5b5ed3adc9
lana.salai <lana.salai@vegait.rs>
2024-10-08 14:09:19 +0200

    Removed QzzyFramework initially created targets, added Core and CoreTests

--- CoreTests/CoreTests.swift
+++ CoreTests/CoreTests.swift
@@ -1,14 +1,14 @@
 //
-//  QzzyFrameworkTests.swift
-//  QzzyFrameworkTests
+//  CoreTests.swift
+//  CoreTests
 //
 //  Created by Lana Salai on 8.10.24..
 //
 
 import XCTest
-@testable import QzzyFramework
+@testable import Core
 
-final class QzzyFrameworkTests: XCTestCase {
+final class CoreTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
